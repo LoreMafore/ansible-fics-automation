206932c9f2102f9974d3223b4e1b642275720cde
Conrad Mercer <conrad.mercer@capcu.org>
2026-02-10 16:30:42 -0600

    WIP

--- plugins/modules/get_ots_schedule_cmr_report.py
+++ plugins/modules/get_ots_schedule_cmr_report.py
@@ -215,16 +215,18 @@ def pdf_to_csv(pdf_path: str, csv_path: str):
             ]):
                 i += 1
                 continue
-            #
-            # if line == 'Investor Codes:':
-            #     all_rows.append([line])
-            #     continue
-            #
-            # if line == 'FIXED-RATE,':
-            #     i += 1
-            #     conjoined_line = line + "\n" + lines[i]
-            #     all_rows.append([conjoined_line])
-            #     continue
+
+            if line == 'Investor Codes:':
+                all_rows.append([line])
+                i += 1
+                continue
+
+            if line == 'FIXED-RATE,':
+                i += 1
+                conjoined_line = line + "\n" + lines[i]
+                all_rows.append([conjoined_line])
+                i += 1
+                continue
 
             # Check if we're at the start of the header
             #and not header_added:
