5c589eb0fd9399dee71016e18aaf70e2a661f61c
Conrad Mercer <conrad.mercer@capcu.org>
2026-02-16 17:04:55 -0600

    Initial CSV implmentation

--- plugins/modules/get_new_loans_entered_report.py
+++ plugins/modules/get_new_loans_entered_report.py
@@ -4,6 +4,8 @@
 #                      David Villafaña <david.villafana@capcu.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
+from dataclasses import dataclass
+from dataclasses import astuple
 from ansible.module_utils.basic import AnsibleModule
 from typing import Callable, Any
 import requests
@@ -11,6 +13,8 @@ import logging
 import os
 import base64
 from datetime import datetime
+import fitz
+import csv
 
 __metaclass__ = type
 
@@ -82,6 +86,26 @@ api_response:
     returned: always
 """
 
+@dataclass(frozen=True)
+class bbox:
+    x0: float
+    y0: float
+    x: float
+    y: float
+    word: str
+    block: int
+    line: int
+    word_no: int
+
+@dataclass
+class PdfLine:
+    linenum: int
+    list[bbox]
+
+
+@dataclass
+class page_representation:
+    lines: list[PdfLine]
 
 def log_function_call(log_path: str, func: Callable[..., Any], *args, **kwargs) -> Any:
     # Ensure the directory for the log file exists
@@ -176,10 +200,31 @@ def get_new_loans_entered_report(
         parameters=params,
     )
 
+def sort_bboxes_into_lines():
+    print("do stuff")
+
+
+def pdf_to_csv(pdf_path: str, csv_path:str):
+    doc = fitz.open(pdf_path)
+    all_rows = []
+
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        text = page.get_text("words")
+        bboxes = [bbox(*x) for x in text]
+        for b in bboxes:
+            all_rows.append(list(astuple(b)))
+
+    doc.close()
+    with open(csv_path, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(all_rows)
+
 
 def run_module():
     module_args = dict(
-        dest=dict(type="str", required=True, no_log=False),
+        pdf_dest=dict(type="str", required=True, no_log=False),
+        csv_dest=dict(type="str", required=True, no_log=False),
         fics_api_url=dict(type="str", required=True, no_log=False),
         api_token=dict(type="str", required=True, no_log=True),
         api_update_database=dict(type="bool", required=True, no_log=False),
@@ -199,11 +244,12 @@ def run_module():
     # supports check mode
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
 
+    pdf_dest: str = module.params["pdf_dest"]
+    csv_dest: str = module.params["csv_dest"]
     api_url: str = module.params["fics_api_url"]
     api_token: str = module.params["api_token"]
     api_update_database: bool = module.params["api_update_database"]
     api_log_directory: str = module.params["api_log_directory"]
-    dest: str = module.params["dest"]
 
     # if the user is working with this module in only check mode we do not
     # want to make any changes to the environment, just return the current
@@ -228,7 +274,7 @@ def run_module():
     try:
         if trial_resp.get("ApiCallSuccessful", None):
             try:
-                os.makedirs(name=str(os.path.dirname(dest)), exist_ok=True)
+                os.makedirs(name=str(os.path.dirname(pdf_dest)), exist_ok=True)
             except Exception as e:
                 module.fail_json(
                     msg=f"failed to create parent directories: {e}",
@@ -242,13 +288,25 @@ def run_module():
                     base64_file = doc.get("DocumentBase64")
 
             if base64_file:
-                port_report = base64.b64decode(base64_file)
-                with open(module.params["dest"], "wb") as port_report_file:
-                    port_report_file.write(port_report)
-                result["changed"] = True
-                result["failed"] = False
-                result["msg"] = f"Wrote file at {module.params['dest']}"
-                result["api_response"] = trial_resp
+                new_loans_entered = base64.b64decode(base64_file)
+                with open(module.params["pdf_dest"], "wb") as new_loans_entered_file:
+                    new_loans_entered_file.write(new_loans_entered)
+
+                try:
+                    pdf_to_csv(pdf_dest, csv_dest)
+                    result["changed"] = True
+                    result["failed"] = False
+                    result["msg"] = f"Wrote file at {module.params['dest']}"
+                    result["api_response"] = trial_resp
+
+                except Exception as e:
+                    module.fail_json(
+                        msg=f"Wrote PDF at {module.params['pdf_dest']} but failed to write CSV: {type(e).__name__}: {str(e)}",
+                        changed=False,
+                        failed=True,
+                        api_response=trial_resp,
+                    )
+                    
             else:
                 result["failed"] = True
                 result["msg"] = "no report file found in api response!"
